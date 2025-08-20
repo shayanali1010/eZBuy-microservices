e44f0e90abf9ca65187cff60d284493bc64cd6f6
shayanali22 <shayanali.1010.official@gmail.com>
2025-08-19 20:33:12 -0700

    Checking for branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MainLayout from "@/layout/MainLayout";
 export default function Home() {
   return (
     <MainLayout>
-      <h1 className="text-black">Welcome to HomePage</h1>
+      <h1 className="text-black">Welcome to Home</h1>
     </MainLayout>
   );
 }
